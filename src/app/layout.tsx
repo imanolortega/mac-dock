888f1699f768bf9da8666d48c3157177c14ec03b
imanol <imanol@easytechgreen.com>
2023-05-07 17:18:21 -0300

    Add metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,43 @@
+import { Metadata } from 'next'
 import './globals.scss'
 
-export const metadata = {
-  title: 'MacOS Dock',
+export const metadata: Metadata = {
+  title: {
+    default: 'MacOS Dock Animation',
+    template: '%s | MacOS Dock Animation',
+  },
   description:
     'A recreation of the macOS dock with NextJS, TypeScript and SCSS 🍏',
+  openGraph: {
+    title: 'MacOS Dock Animation',
+    description:
+      'A recreation of the macOS dock with NextJS, TypeScript and SCSS 🍏',
+    url: 'https://mac-dock.vercel.app/',
+    siteName: 'Imanol Ortega',
+    images: [
+      {
+        url: 'https://mac-dock.vercel.app/images/og.png',
+        width: 1920,
+        height: 1080,
+      },
+    ],
+    locale: 'es-ES',
+    type: 'website',
+  },
+  robots: {
+    index: true,
+    follow: true,
+    googleBot: {
+      index: true,
+      follow: true,
+      'max-video-preview': -1,
+      'max-image-preview': 'large',
+      'max-snippet': -1,
+    },
+  },
+  icons: {
+    shortcut: '/favicon.ico',
+  },
 }
 
 export default function RootLayout({
